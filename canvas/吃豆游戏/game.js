19343458ac4a9a3e39498cc8bb2de7acc3f9ce87
lanhe233 <916039201@qq.com>
2017-07-06 18:15:31 +0800

    2017/7/6

--- canvas/吃豆游戏/game.js
+++ canvas/吃豆游戏/game.js
@@ -5,7 +5,6 @@
 
 function Game(id, options) {
 
-
 	let _this = this
 
 	Object.assign(this, {
@@ -13,17 +12,80 @@ function Game(id, options) {
 		_height: 600
 	}, options)
 
+	let _cvs = document.getElementById(id)
+	_cvs.width = _this._width
+	_cvs.height = _this._height
+	let _ctx = _cvs.getContext('2d')
 
-	let cvs = document.getElementById(id)
-	cvs.width = _this._width
-	cvs.height = _this._height
-	let ctx = cvs.getContext('2d')
-
-	let Stage = {
-		id: 0,					//舞台编号
-		status: 0,			//舞台状态：0 未激活 | 1 激活中
-
+	let Stage = function (params) {
+		Object.assign(this, {
+			id: 0,					//舞台编号
+			status: 0,			//舞台状态：0 未激活 | 1 激活中
+			maps: [],
+			items: []
+		}, params)
 	}
+
+	let Map = function (params) {
+		Object.assign(this, {
+			id: 0,					//地图编号
+			status: 0,			//地图状态：0 未激活 | 1 激活中
+			stage: null,		//该地图绑定的舞台
+			data: null,			//地图数据
+			draw () {},
+			update () {}
+		}, params)
+	}
+
+	let Item = function (params) {
+		Object.assign(this, {
+			id: 0,					//物体编号
+			status: 0,			//物体状态：0 未激活 | 1 正常 | 2 死亡 | 3 特殊
+			stage: null,		//该物体绑定的舞台
+			life: 3,				//物体生命
+			x: 0,
+			y: 0,
+			width: 20,
+			height: 20,
+			draw () {},
+			update () {}
+		}, params)
+	}
+
+	let _stages = [],	//舞台数组
+			_index = 0		//当前位于哪个舞台（舞台数组下标）
+			// _items = []
+
+	Stage.prototype.createMap =  function (params) {
+		
+	}
+
+	Stage.prototype.createItem = function (params) {
+		let item = new Item(params)
+		item.id = this.items.length
+		this.items.push(item)
+		// item.stage = this
+		// return item
+	}
+
+	this.createStage = function (params) {
+		let stage = new Stage(params)
+		_index = stage.id = _stages.length
+		_stages.push(stage)
+		return stage
+	}
+
+	this.start = function () {
+		_stages[_index].items.forEach(function(item, i, arr){
+			item.draw(_ctx)
+		})
+	}
+
+	this.init = function () {
+		_index = 0
+		this.start()
+	}
+
 
 
 
--- canvas/吃豆游戏/index.js
+++ canvas/吃豆游戏/index.js
@@ -1,3 +1,110 @@
 
 
-let game = new Game('canvas')
+/*
+let cvs = document.getElementById('canvas')
+cvs.width = 960
+cvs.height = 600
+let ctx = cvs.getContext('2d')
+
+
+// 画player
+ctx.fillStyle = '#ffe600'
+ctx.beginPath()
+ctx.arc(20, 20, 18, 0.25*Math.PI, (2-0.25)*Math.PI)
+ctx.lineTo(20, 20)
+ctx.fill()
+
+// 画有眼睛的player
+ctx.fillStyle = '#ffe600'
+ctx.beginPath()
+ctx.arc(180, 60, 50, 0.25*Math.PI, (2-0.25)*Math.PI)
+ctx.lineTo(180, 60)
+ctx.fill()
+
+ctx.fillStyle = '#000'
+ctx.beginPath()
+ctx.arc(180+5, 60-25, 7, 0, Math.PI*2)
+ctx.fill()
+
+
+// 画文字
+ctx.fillStyle = '#fff'
+ctx.font = '36px blod'
+ctx.textAlign = 'center'
+ctx.fillText('Pac-Man', ctx.canvas.width/2, 50)
+*/
+
+window.onload = function() {
+	let game = new Game('canvas')
+
+	let stage = game.createStage()
+
+	// 画logo
+	stage.createItem({
+		x: game._width/2,
+		y: game._height*0.4,
+		width: 100,
+		height: this.width,
+		draw (ctx) {
+			// 画player
+			let rate = 0.16
+			ctx.fillStyle = '#ffe600'
+			ctx.beginPath()
+			ctx.arc(this.x, this.y, this.width/2, rate*Math.PI, (2-rate)*Math.PI)
+			ctx.lineTo(this.x, this.y)
+			ctx.fill()
+
+			// 画眼睛
+			let r = this.width*0.07
+			ctx.fillStyle = '#000'
+			ctx.beginPath()
+			ctx.arc(this.x+r-2, this.y-this.width/4, r, 0, Math.PI*2)
+			ctx.fill()
+		}
+	})
+	// 画游戏名
+	stage.createItem({
+		x: game._width/2,
+		y: game._height*0.6,
+		draw (ctx) {
+			ctx.fillStyle = '#fff'
+			ctx.font = '36px bold Helvetica'
+			ctx.textAlign = 'center'
+			ctx.fillText('Pac-Man', this.x, this.y)
+		}
+	})
+	// 画提示文字
+	stage.createItem({
+		x: game._width/2,
+		y: game._height*0.96,
+		draw (ctx) {
+			ctx.fillStyle = '#fff'
+			ctx.font = '16px Source Code Pro'
+			ctx.textAlign = 'center'
+			ctx.fillText('按[空格]暂停或继续', this.x, this.y)
+		}
+	})
+
+	game.init()
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
